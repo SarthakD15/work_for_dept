6f0e7f5221073b4bb220d69d609819ab5a2886c5
Sarthak Deshmukh <sarthakd1512@gmail.com>
2023-01-09 22:44:00 +0530

    Dropdown & form changed

--- actServer/public/javascripts/index.js
+++ actServer/public/javascripts/index.js
@@ -1,46 +1,9 @@
-// let subMenu = document.getElementById("subMenu");
-
-// function toggleMenu(){
-//   subMenu.classList.toggle("open-menu");
-// }
-
-
-// const selected = document.querySelector(".selected");
-// const optionsContainer = document.querySelector(".options-container");
-// const searchBox = document.querySelector(".search-box input");
-
-// const optionsList = document.querySelectorAll(".option");
-
-// selected.addEventListener("click", () => {
-//   optionsContainer.classList.toggle("active");
-
-//   searchBox.value = "";
-//   filterList("");
-
-//   if (optionsContainer.classList.contains("active")) {
-//     searchBox.focus();
-//   } 
-// });
-
-// optionsList.forEach(o => {
-//   o.addEventListener("click", () => {
-//     selected.innerHTML = o.querySelector("label").innerHTML;
-//     optionsContainer.classList.remove("active");
-//   });
-// });
-
-// searchBox.addEventListener("keyup", function(e) {
-//   filterList(e.target.value);
-// });
-
-// const filterList = searchTerm => {
-//   searchTerm = searchTerm.toLowerCase();
-//   optionsList.forEach(option => {
-//     let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
-//     if (label.indexOf(searchTerm) != -1) {
-//       option.style.display = "block";
-//     } else {
-//       option.style.display = "none";
-//     }
-//   });
-// };
+document.getElementById('search-button').addEventListener('click', function() {
+    var dropdown1Value = document.getElementById('dropdown1').value;
+    var dropdown2Value = document.getElementById('dropdown2').value;
+    var dropdown3Value = document.getElementById('dropdown3').value;
+  
+    console.log('Dropdown 1 value: ' + dropdown1Value);
+    console.log('Dropdown 2 value: ' + dropdown2Value);
+    console.log('Dropdown 3 value: ' + dropdown3Value);
+  });
